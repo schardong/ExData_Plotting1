fd0a0969960c7a5a10a590739bd54f3277d47413
Guilherme G. Schardong <schardong.gg@gmail.com>
2016-12-31 15:43:52 -0200

    Downloading the dataset automatically now. Forgot to change some calls in the plot4.R script, not it produces the plots correctly.

--- plot3.R
+++ plot3.R
@@ -1,5 +1,14 @@
 library(dplyr)
 
+url.dataset <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+filepath = "household_power_consumption.zip"
+
+if (!file.exists("household_power_consumption.txt")) {
+  download.file(url=url.dataset, destfile=filepath)
+  unzip(filepath)
+  file.remove(filepath)
+}
+
 ## Reading and subsetting the data to get only the rows with days 2007-02-01 and
 ## 2007-02-02.
 input.data <- read.table(file='household_power_consumption.txt',
--- plot4.R
+++ plot4.R
@@ -1,5 +1,14 @@
 library(dplyr)
 
+url.dataset <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+filepath = "household_power_consumption.zip"
+
+if (!file.exists("household_power_consumption.txt")) {
+  download.file(url=url.dataset, destfile=filepath)
+  unzip(filepath)
+  file.remove(filepath)
+}
+
 ## Reading and subsetting the data to get only the rows with days 2007-02-01 and
 ## 2007-02-02.
 input.data <- read.table(file='household_power_consumption.txt',
@@ -17,7 +26,8 @@ input.data <- read.table(file='household_power_consumption.txt',
                                       'numeric'))
 
 data <- filter(input.data, Date == '1/2/2007' | Date == '2/2/2007') %>%
-  mutate(Date = as.Date(strptime(Date, '%d/%m/%Y')))
+  mutate(Datetime = paste(Date, Time, sep = ' ')) %>%
+  mutate(Datetime = as.POSIXct(strptime(Datetime, '%d/%m/%Y %H:%M:%S')))
 
 png('plot4.png', width=480, height=480)
 par(mfrow=c(2,2))
@@ -42,7 +52,7 @@ plot(Sub_metering_1 ~ Datetime,
      ylab='Energy sub metering')
 lines(x=data$Datetime, y=data$Sub_metering_2, col=2)
 lines(x=data$Datetime, y=data$Sub_metering_3, col=4)
-legend('topright', legend=names(data)[7:9], col=c(1,2,4), lwd=1)
+legend('topright', legend=names(data)[7:9], col=c(1,2,4), lwd=1, bty='n')
 ## Plot 4
 plot(Global_reactive_power ~ Datetime,
      data=data,
